20b83fbee8b45252e64ac4c2b034b4bf6371ff36
j1nn0 <j1nn0.github@gmail.com>
2025-09-09 09:13:50 +0900

    💚 Add vite-plugin-eslint

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,20 @@
 import { URL, fileURLToPath } from 'node:url';
 
 import { defineConfig } from 'vite';
+import eslint from 'vite-plugin-eslint';
 import vue from '@vitejs/plugin-vue';
 import vueDevTools from 'vite-plugin-vue-devtools';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueDevTools()],
+  plugins: [
+    vue(),
+    vueDevTools(),
+    eslint({
+      failOnWarning: true,
+      include: ['src/**/*.js', 'src/**/*.vue'],
+    }),
+  ],
   publicDir: false,
   resolve: {
     alias: {
